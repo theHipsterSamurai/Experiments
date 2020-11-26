0b3b48605753a7eb924c378f9fd9e8df172a4429
theHipsterSamurai <63050520+theHipsterSamurai@users.noreply.github.com>
2020-11-26 17:00:31 +0100

    Add files via upload

---
+++ Mole Calculator.py
@@ -0,0 +1,7 @@
+import tkinter
+window = tkinter.Tk()
+
+window.title("The Marvelous Mole Calculator")
+
+label = tkinter.Label(window, text = "Welcome to the Marvelous Mole Calculator")
+window.mainloop()
